dc15b27a845caab2525bf0737446ad8b332c4ae9
Shaddu413 <55288540+Shaddu413@users.noreply.github.com>
2022-05-25 20:13:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 23 21:52:38 2022
+
+@author: Shahood
+"""
+import nltk
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    nltk.download('vader_lexicon')
+    from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    sid = SentimentIntensityAnalyzer()
+    score = ((sid.polarity_scores(str(text))))['compound']
+
+    if(score > 0):
+        label = 'This sentence is positive'
+    elif(score == 0):
+        label = 'This sentence is neutral'
+    else:
+        label = 'This sentence is negative'
+
+    return(render_template('index.html', variable=label))
+
+if __name__ == "__main__":
+    app.run(port='8088', threaded=False, debug=False)
